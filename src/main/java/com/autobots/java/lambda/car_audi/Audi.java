5c470b881386d1a7b7637882a30e8a2e7fbcc883
Aidai <aidai0296@gmail.com>
2025-07-07 16:50:50 +0600

    fixed pull request comments

--- src/main/java/com/autobots/java/lambda/car_audi/Audi.java
+++ src/main/java/com/autobots/java/lambda/car_audi/Audi.java
@@ -1,4 +1,7 @@
 package com.autobots.java.lambda.car_audi;
 
 public class Audi {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
 }
--- src/main/java/com/autobots/java/lambda/car_audi/BMW.java
+++ src/main/java/com/autobots/java/lambda/car_audi/BMW.java
@@ -1,4 +1,7 @@
 package com.autobots.java.lambda.car_audi;
 
 public class BMW {
+    public static void main(String[] args) {
+        System.out.println("BMW ");
+    }
 }

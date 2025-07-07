2de214bf1db8129a49161b89fc16d943da963505
Aidai <aidai0296@gmail.com>
2025-07-07 17:15:34 +0600

    Visa-card

--- src/main/java/com/autobots/java/lambda/payment/VisaPayment.java
+++ src/main/java/com/autobots/java/lambda/payment/VisaPayment.java
@@ -1,4 +1,7 @@
 package com.autobots.java.lambda.payment;
 
 public class VisaPayment {
+    public void payByVisaCard(double amount){
+        System.out.println("Visa payment by visa card");
+    }
 }

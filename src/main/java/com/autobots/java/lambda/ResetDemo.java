31c106ffe0585cada564f3e80a875725b25aba02
Aidai <aidai0296@gmail.com>
2025-07-08 09:24:04 +0600

    version1.0.1

--- src/main/java/com/autobots/java/lambda/ResetDemo.java
+++ src/main/java/com/autobots/java/lambda/ResetDemo.java
@@ -1,4 +1,8 @@
 package com.autobots.java.lambda;
 
 public class ResetDemo {
+    public static void main(String[] args) {
+        System.out.println("Version1.0.0");
+        System.out.println("Version1.0.1");
+    }
 }
